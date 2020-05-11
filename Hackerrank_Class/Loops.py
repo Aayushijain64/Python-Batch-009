1d8b97637b327c415d92775e9e3d80f2c33cfa13
Aayushi_jain64 <aayushijain6451@gmail.com>
2020-05-12 01:57:27 +0530

    Hackerrank Class Questions

---
+++ Hackerrank_Class/Loops.py
@@ -0,0 +1,6 @@
+#Sum Of Squares
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        sqr = i*i
+        print(sqr)
---
+++ Hackerrank_Class/PrintFunction.py
@@ -0,0 +1,5 @@
+#Print 123....n where n is user input
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end='')
